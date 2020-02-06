6776010d43f3435525d124e1535d279a89288ca1
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2020-02-06 06:42:34 -0800

    Prettier Reformat
    - Reformats all source files with Prettier.

--- src/common/css-helpers.ts
+++ src/common/css-helpers.ts
@@ -1,11 +1,14 @@
 export function getCssColor(variableName: string): string {
-    return getComputedStyle(document.documentElement)
-        .getPropertyValue(variableName)
-        .trim();
+  return getComputedStyle(document.documentElement)
+    .getPropertyValue(variableName)
+    .trim();
 }
 
 export function getCssNumber(variableName: string): number {
-    return parseInt(getComputedStyle(document.documentElement)
-        .getPropertyValue(variableName)
-        .trim(), 10);
+  return parseInt(
+    getComputedStyle(document.documentElement)
+      .getPropertyValue(variableName)
+      .trim(),
+    10
+  );
 }
--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -1,118 +1,136 @@
 import { createMuiTheme, Theme, ThemeProvider } from '@material-ui/core/styles';
 import { getCssColor, getCssNumber } from 'common/css-helpers';
 import { IpcChannels } from 'common/ipc-channels';
-import { createLyricistantLanguage, createLyricistantTheme } from 'common/monaco-helpers';
+import {
+  createLyricistantLanguage,
+  createLyricistantTheme
+} from 'common/monaco-helpers';
 import { Rhyme } from 'common/Rhyme';
 import { ipcRenderer } from 'electron';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import React, { Fragment, FunctionComponent, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Subject } from 'rxjs';
 import { Editor, TextReplacement, WordAtPosition } from './Editor';
 import { Rhymes } from './Rhymes';
 
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 createLyricistantLanguage();
 
-const createTheme = (): Theme => createMuiTheme({
+const createTheme = (): Theme =>
+  createMuiTheme({
     palette: {
-        action: {
-            hover: getCssColor('--primary-color'),
-            hoverOpacity: 0
-        },
-        background: { default: getCssColor('--primary-background-color') },
-        text: {
-            primary: getCssColor('--primary-text-color'),
-            secondary: getCssColor('--secondary-text-color')
-        },
+      action: {
+        hover: getCssColor('--primary-color'),
+        hoverOpacity: 0
+      },
+      background: { default: getCssColor('--primary-background-color') },
+      text: {
+        primary: getCssColor('--primary-text-color'),
+        secondary: getCssColor('--secondary-text-color')
+      }
     },
     typography: {
-        fontSize: getCssNumber('--details-text-size')
+      fontSize: getCssNumber('--details-text-size')
     }
-});
+  });
 
 export interface AppProps {
-    onShouldUpdateBackground: (newBackground: string) => void;
+  onShouldUpdateBackground: (newBackground: string) => void;
 }
 
 const selectedWords: Subject<WordAtPosition> = new Subject();
 const textReplacements: Subject<TextReplacement> = new Subject();
 const onWordSelected: (word: WordAtPosition) => void = (word) => {
-    selectedWords.next(word);
+  selectedWords.next(word);
 };
 
-const onRhymeClicked: (rhyme: Rhyme, range: monaco.IRange) => void = (rhyme, range) => {
-    textReplacements.next({ word: rhyme.word, range });
+const onRhymeClicked: (rhyme: Rhyme, range: monaco.IRange) => void = (
+  rhyme,
+  range
+) => {
+  textReplacements.next({ word: rhyme.word, range });
 };
 
 export const App: FunctionComponent<AppProps> = (props: AppProps) => {
-    const [theme, setTheme] = useState(createTheme());
-    const fontSize = getCssNumber('--editor-text-size');
+  const [theme, setTheme] = useState(createTheme());
+  const fontSize = getCssNumber('--editor-text-size');
 
-    useEffect(handleThemeChanges(setTheme, props.onShouldUpdateBackground), []);
-    useEffect(handleFileChanges(), []);
+  useEffect(handleThemeChanges(setTheme, props.onShouldUpdateBackground), []);
+  useEffect(handleFileChanges(), []);
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Fragment>
-                <div id={'editor'}>
-                    <Editor fontSize={fontSize} onWordSelected={onWordSelected} textReplacements={textReplacements} />
-                </div>
-                <div id={'detail-column'} >
-                    <Rhymes queries={selectedWords} onRhymeClicked={onRhymeClicked} />
-                </div>
-                <ToastContainer autoClose={3000} />
-            </Fragment>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Fragment>
+        <div id={'editor'}>
+          <Editor
+            fontSize={fontSize}
+            onWordSelected={onWordSelected}
+            textReplacements={textReplacements}
+          />
+        </div>
+        <div id={'detail-column'}>
+          <Rhymes queries={selectedWords} onRhymeClicked={onRhymeClicked} />
+        </div>
+        <ToastContainer autoClose={3000} />
+      </Fragment>
+    </ThemeProvider>
+  );
 };
 
 function handleThemeChanges(
-    setTheme: (theme: Theme) => void,
-    onShouldUpdateBackground: (newBackground: string) => void
+  setTheme: (theme: Theme) => void,
+  onShouldUpdateBackground: (newBackground: string) => void
 ): () => void {
-    return () => {
-        const darkModeChangedListener = (_: any, useDarkTheme: boolean) => {
-            if (useDarkTheme) {
-                document.documentElement.setAttribute('data-theme', 'dark');
-            } else {
-                document.documentElement.setAttribute('data-theme', 'light');
-            }
+  return () => {
+    const darkModeChangedListener = (_: any, useDarkTheme: boolean) => {
+      if (useDarkTheme) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+      } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+      }
 
-            const appTheme = createTheme();
-            setTheme(appTheme);
-            monaco.editor.setTheme(createLyricistantTheme(useDarkTheme));
-            onShouldUpdateBackground(appTheme.palette.background.default)
-        };
-        ipcRenderer.on(IpcChannels.THEME_CHANGED, darkModeChangedListener);
-        ipcRenderer.send(IpcChannels.READY_FOR_EVENTS);
-
-        return function cleanup() {
-            ipcRenderer.removeListener(IpcChannels.THEME_CHANGED, darkModeChangedListener);
-        };
+      const appTheme = createTheme();
+      setTheme(appTheme);
+      monaco.editor.setTheme(createLyricistantTheme(useDarkTheme));
+      onShouldUpdateBackground(appTheme.palette.background.default);
     };
+    ipcRenderer.on(IpcChannels.THEME_CHANGED, darkModeChangedListener);
+    ipcRenderer.send(IpcChannels.READY_FOR_EVENTS);
+
+    return function cleanup() {
+      ipcRenderer.removeListener(
+        IpcChannels.THEME_CHANGED,
+        darkModeChangedListener
+      );
+    };
+  };
 }
 
 function handleFileChanges(): () => void {
-    return () => {
-        const onFileOpened = (_: any, error: any, fileName: string, data: string) => {
-            if (error) {
-                // todo show error message
-            } else {
-                document.title = fileName;
-            }
-        };
-        ipcRenderer.on(IpcChannels.FILE_OPENED, onFileOpened);
-
-        const onNewFile = () => {
-            document.title = 'Untitled';
-        };
-        ipcRenderer.on(IpcChannels.NEW_FILE_CREATED, onNewFile);
-
-        return function cleanup() {
-            ipcRenderer.removeListener(IpcChannels.FILE_OPENED, onFileOpened);
-            ipcRenderer.removeListener(IpcChannels.NEW_FILE_CREATED, onNewFile);
-        };
+  return () => {
+    const onFileOpened = (
+      _: any,
+      error: any,
+      fileName: string,
+      data: string
+    ) => {
+      if (error) {
+        // todo show error message
+      } else {
+        document.title = fileName;
+      }
     };
+    ipcRenderer.on(IpcChannels.FILE_OPENED, onFileOpened);
+
+    const onNewFile = () => {
+      document.title = 'Untitled';
+    };
+    ipcRenderer.on(IpcChannels.NEW_FILE_CREATED, onNewFile);
+
+    return function cleanup() {
+      ipcRenderer.removeListener(IpcChannels.FILE_OPENED, onFileOpened);
+      ipcRenderer.removeListener(IpcChannels.NEW_FILE_CREATED, onNewFile);
+    };
+  };
 }
