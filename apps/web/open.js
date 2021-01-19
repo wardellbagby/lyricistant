f355803f07bd02a53c513ab14d3104f741fff3b6
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2021-01-19 08:43:00 -0800

    Web - Use Web Server
    - Uses a basic web server for developing the Web app.

--- apps/web/open.js
+++
@@ -1,9 +0,0 @@
-const open = require('open');
-const path = require('path');
-
-(async () => {
-    const file = path.resolve(process.env['BUILD_WORKSPACE_DIRECTORY'], process.argv[2]);
-    console.log('Opening ' + file);
-    await open(file);
-  }
-)();
