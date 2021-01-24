163d524e6c3de279ff919b4eee6b218f0285a6e6
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2021-01-24 15:22:48 -0800

    Web - Preferences Saving Fix
    - Fixes an issue where Web preferences didn't properly save.

--- apps/web/main/platform/Preferences.ts
+++ apps/web/main/platform/Preferences.ts
@@ -2,11 +2,11 @@ import { Preferences as IPreferences } from '@common/preferences/Preferences';
 import { PreferencesData } from '@common/preferences/PreferencesData';
 
 export class WebPreferences implements IPreferences {
-  private readonly prefsKey = 'lyricist_preferences';
+  private readonly prefsKey = 'lyricistant_preferences';
   public setPreferences: (data: PreferencesData) => void = (data) =>
     localStorage.setItem(this.prefsKey, JSON.stringify(data));
-  public getPreferences: () => PreferencesData | undefined = () => {
-    const savedPrefs = localStorage.getItem('lyricistant_preferences');
+  public getPreferences = () => {
+    const savedPrefs = localStorage.getItem(this.prefsKey);
     if (savedPrefs) {
       return JSON.parse(savedPrefs);
     } else {
