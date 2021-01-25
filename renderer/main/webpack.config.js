6bb93316e604319839ff33bfac9be5ded21a7ba5
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2021-01-24 20:50:36 -0800

    Renderer - Bazel Cleanup
    - Gets rid of the HTML file generation, since it's very simple now.
    - Has the renderer handle bundling itself, since outside of Web, most
    other platforms would prefer that.

--- renderer/main/webpack.config.js
+++ renderer/main/webpack.config.js
@@ -1,14 +1,8 @@
 const { DefinePlugin } = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const packageInfo = require('@lyricistant/package_info.json');
 
-module.exports = ({ htmlTemplate }) => ({
+module.exports = () => ({
     plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Untitled',
-        template: htmlTemplate,
-        inject: false
-      }),
       new DefinePlugin({
         'process.env.APP_VERSION': JSON.stringify(packageInfo.version),
         'process.env.APP_HOMEPAGE': JSON.stringify(packageInfo.homepage),
