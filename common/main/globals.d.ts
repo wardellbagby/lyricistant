634338ed618d9c7a635b14884d4c02e53ad59e10
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2021-01-25 19:58:37 -0800

    Renderer - Move to renderer/page/
    - Moves all the current renderer code to renderer/page to make it easier
    to split renderer into multiple packages.

--- common/main/globals.d.ts
+++ common/main/globals.d.ts
@@ -2,9 +2,10 @@ import type { Analytics } from './analytics/Analytics';
 import type { PlatformDelegate } from './Delegates';
 import { Logger } from './Logger';
 
+declare type RendererLogger = Omit<Logger, 'save'>;
 declare global {
   interface Window {
-    logger: Omit<Logger, 'save'>;
+    logger: RendererLogger;
     goatcounter?: Analytics;
     platformDelegate: PlatformDelegate;
   }
--- renderer/page/main/globals.ts
+++ renderer/page/main/globals.ts
@@ -1,4 +1,5 @@
 import { PlatformDelegate } from '@common/Delegates';
+import { RendererLogger } from "@common/globals";
 
 const [APP_VERSION, APP_HOMEPAGE, APP_AUTHOR] = [
   process.env.APP_VERSION,
@@ -6,7 +7,7 @@ const [APP_VERSION, APP_HOMEPAGE, APP_AUTHOR] = [
   process.env.APP_AUTHOR,
 ];
 
-const logger = window.logger;
+const logger: RendererLogger = window.logger;
 const platformDelegate: PlatformDelegate = window.platformDelegate;
 
 export { APP_AUTHOR, APP_HOMEPAGE, APP_VERSION, logger, platformDelegate };
