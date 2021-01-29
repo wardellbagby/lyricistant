dda8f67e806739f85c95d712e48ff799c365cea9
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2021-01-29 10:08:18 -0800

    CodeMirror 6 - Find & Replace Panel
    - Adds some basic CSS to the Find & Replace panel in CodeMirror 6 so it
    fits more inline with the rest of the app.

--- renderer/codemirror/main/editorTheme.ts
+++ renderer/codemirror/main/editorTheme.ts
@@ -23,7 +23,7 @@ export const editorTheme = (appTheme: Theme): Extension =>
         },
       },
       $scroller: {
-        fontFamily: '"Roboto Mono"',
+        fontFamily: '"Roboto"',
         fontSize: appTheme.typography.fontSize,
       },
       $gutters: {
@@ -32,6 +32,68 @@ export const editorTheme = (appTheme: Theme): Extension =>
         backgroundColor: appTheme.palette.background.default,
         color: appTheme.palette.text.secondary,
         borderRight: 'none',
+      },
+      $panel: {
+        backgroundColor: appTheme.palette.background.paper,
+        color: appTheme.palette.getContrastText(
+          appTheme.palette.background.paper
+        ),
+      },
+      $searchMatch: {
+        backgroundColor: appTheme.palette.grey.A100,
+        color: appTheme.palette.getContrastText(appTheme.palette.grey.A100),
+      },
+      '$searchMatch.selected': {
+        backgroundColor: appTheme.palette.primary.main,
+        color: appTheme.palette.primary.contrastText,
+      },
+      $button: {
+        background: appTheme.palette.primary.main,
+        color: appTheme.palette.primary.contrastText,
+        border: 'none',
+        borderRadius: '0px',
+        margin: '2px',
+        padding: '8px',
+        width: '100px',
+        height: '35px',
+        textAlign: 'center',
+        textDecoration: 'none',
+        display: 'inline-block',
+        fontFamily: '"Roboto"',
+        fontSize: appTheme.typography.fontSize,
+        '&:focus': {
+          outline: 'none',
+        },
+        '&:active': {
+          background: appTheme.palette.action.active,
+        },
+      },
+      $textfield: {
+        color: appTheme.palette.text.primary,
+        padding: '8px',
+        width: '150px',
+        height: '35px',
+        textDecoration: 'none',
+        fontFamily: '"Roboto"',
+        fontSize: appTheme.typography.fontSize,
+        '&:focus': {
+          outline: 'none',
+        },
+      },
+      '$panel label': {
+        display: 'none',
+        fontFamily: '"Roboto"',
+        fontSize: appTheme.typography.fontSize,
+      },
+      '$panel.search button[name = "select"]': {
+        display: 'none',
+      },
+      '$panel.search button[name = "close"]': {
+        fontSize: '24px',
+        right: '8px',
+        '&:focus': {
+          outline: 'none',
+        },
       },
     },
     {
