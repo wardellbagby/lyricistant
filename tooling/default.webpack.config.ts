415036826eb9304a753a3084187ab546924ef590
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2021-04-03 09:12:54 -0700

    Web - Sourcemap....again.
    - Fixes broken sourcemaps....again.

--- tooling/default.webpack.config.ts
+++ tooling/default.webpack.config.ts
@@ -2,7 +2,22 @@ import { Mode } from '@tooling/common-tasks.gulp';
 import webpack, { Configuration } from 'webpack';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
+const webpackMode = (mode: Mode): Configuration['mode'] => {
+  if (mode === 'test') {
+    return 'production';
+  }
+  return mode;
+};
+const devtool = (mode: Mode): Configuration['devtool'] => {
+  if (mode === 'development' || mode === 'test') {
+    return 'eval-cheap-module-source-map';
+  }
+  return 'source-map';
+};
+
 export default (mode: Mode): Configuration => ({
+  mode: webpackMode(mode),
+  devtool: devtool(mode),
   resolve: {
     plugins: [new TsconfigPathsPlugin()],
     extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.wasm'],
