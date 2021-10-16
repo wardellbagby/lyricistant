833a09b9949315e20a0b1d0e70827e755885c448
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2021-10-16 09:21:22 -0500

    ci: use @wardellbagby/gh-workflow-gen for workflow generation
    Did I write a whole new library just to write this commit message? Yes. Yes, I did.

--- .github/workflow-templates/helpers/Workflow.ts
+++ .github/workflow-templates/helpers/Workflow.ts
@@ -1,36 +1,10 @@
+import {
+  Job as RealJob,
+  Step as RealStep,
+  Workflow as RealWorkflow,
+} from '@wardellbagby/gh-workflow-gen';
 import * as versions from './versions';
 
-interface BranchesTagsOrPaths {
-  branches?: string[];
-  tags?: string[];
-  paths?: string[];
-}
-
-interface Schedule {
-  cron: string;
-}
-export interface Job {
-  name: string;
-  'runs-on': 'ubuntu-20.04' | 'macos-10.15';
-  needs?: Array<string | Job> | string | Job;
-  if?: string;
-  steps: Step[];
-}
-export interface Step {
-  name: string;
-  if?: string;
-  uses?: typeof versions[Extract<keyof typeof versions, string>];
-  run?: string;
-  with?: Record<string, string | boolean | number>;
-  env?: Record<string, string | boolean>;
-}
-export interface Workflow {
-  name: string;
-  on: {
-    push?: BranchesTagsOrPaths;
-    pull_request?: BranchesTagsOrPaths;
-    schedule?: Schedule[];
-    workflow_dispatch?: Record<string, never>;
-  };
-  jobs: Record<string, Job>;
-}
+export type Job = RealJob<typeof versions>;
+export type Step = RealStep<typeof versions>;
+export type Workflow = RealWorkflow<typeof versions>;
