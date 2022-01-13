4c91e715744e9554207c4427c7638e70a50419f4
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-01-13 12:04:42 -0800

    fix: use legacy non-worker Web flow for non-Chrome and non-Firefox

--- apps/web/main/index.ts
+++ apps/web/main/index.ts
@@ -1,4 +1,52 @@
-import { start } from '@web-renderer/index';
+import { start as startNew } from '@web-renderer/index';
+import { isChrome, isFirefox } from 'react-device-detect';
+import { Logger } from '@lyricistant/common/Logger';
+import { platformDelegate } from '@lyricistant/core-platform/Delegates';
+import { Managers } from '@lyricistant/common/Managers';
+import {
+  createBaseComponent,
+  createBaseManagers,
+} from '@lyricistant/core-platform/AppComponent';
+import { UnloadManager } from '@lyricistant/core-platform/platform/UnloadManager';
+
+const startLegacy = async () => {
+  const appComponent = createBaseComponent();
+  appComponent.registerSingleton<UnloadManager>();
+  appComponent.registerSingleton<Managers>(() => [
+    ...createBaseManagers(appComponent),
+    () => appComponent.get<UnloadManager>(),
+  ]);
+
+  const logger = appComponent.get<Logger>();
+
+  window.logger = logger;
+  window.platformDelegate = platformDelegate;
+
+  return new Promise<void>((resolve) => {
+    appComponent.get<Managers>().forEach((manager) => manager().register());
+    resolve();
+  })
+    .then(async () => {
+      logger.info('Platform information', {
+        appPlatform: 'Web - Legacy',
+        version:
+          (await import('@lyricistant/renderer/globals')).APP_VERSION ??
+          'Error',
+        userAgent: navigator.userAgent,
+      });
+    })
+    .then(() => import('@lyricistant/renderer/index'))
+    .catch((reason) => {
+      throw Error(`Could not load the renderer page: ${reason}`);
+    });
+};
+
+const start = async () => {
+  if (isChrome || isFirefox) {
+    return startNew();
+  }
+  return startLegacy();
+};
 
 start().catch((reason: any) => {
   if (reason instanceof Error) {
