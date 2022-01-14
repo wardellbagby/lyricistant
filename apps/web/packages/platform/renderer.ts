74ae0bea6cb366110217a23e9e5c51cb0f223dcf
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-01-14 13:58:59 -0800

    build: ignore sporadic web platform worker typing issue

--- apps/web/packages/platform/renderer.ts
+++ apps/web/packages/platform/renderer.ts
@@ -1,4 +1,7 @@
 import { wrap } from 'comlink';
 import { Renderer } from '@web-common/Renderer';
 
-export const renderer: Renderer = wrap(self);
+// TODO Webpack sometimes thinks self is a Window instead of a
+// WorkerGlobalScope. Figure out why that happens.
+
+export const renderer: Renderer = wrap(self as any);
