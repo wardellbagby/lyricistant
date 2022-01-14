35d9be3be7c013f6901308983f8f1944b8dfd908
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-01-14 09:38:41 -0800

    build: increment mobile versions on release publish, not deploy

--- scripts/publish_release.ts
+++ scripts/publish_release.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env -S node -r ./register-ts-node
 import { spawnSync, SpawnSyncReturns } from 'child_process';
+import fs from 'fs';
 import inquirer from 'inquirer';
 
 const requireSuccessful = <T = string | Buffer>(
@@ -7,8 +8,15 @@ const requireSuccessful = <T = string | Buffer>(
   onFailure: (result: SpawnSyncReturns<T>) => void
 ) => {
   if (result.error || result.status !== 0) {
-    console.log(result.stdout.toString());
-    console.error(result.stderr.toString());
+    if (result.stdout) {
+      console.log(result.stdout.toString());
+    }
+    if (result.stderr) {
+      console.log(result.stderr.toString());
+    }
+    if (result.error) {
+      console.error(result.error.message);
+    }
     onFailure(result);
     process.exit(result.status ?? 1);
   }
@@ -17,6 +25,42 @@ const updateChangelog = () => {
   requireSuccessful(spawnSync('./node_modules/.bin/standard-changelog'), () => {
     console.error('Failed to create changelog');
   });
+};
+
+const updateMobileAppVersions = (version: string) => {
+  requireSuccessful(
+    spawnSync('xcrun agvtool next-version -all', {
+      cwd: 'apps/mobile/ios/App',
+      shell: true,
+    }),
+    () => {
+      console.error('Failed to update iOS build number');
+    }
+  );
+  requireSuccessful(
+    spawnSync(`xcrun agvtool new-marketing-version ${version}`, {
+      cwd: 'apps/mobile/ios/App',
+      shell: true,
+    }),
+    () => {
+      console.error('Failed to update iOS version string');
+    }
+  );
+
+  const androidVersions = fs
+    .readFileSync('apps/mobile/android/app/versions.properties', 'utf8')
+    .split('\n');
+  const versionCode =
+    Number(
+      androidVersions
+        .find((line) => line.startsWith('versionCode='))
+        .split('=')[1]
+    ) + 1;
+
+  fs.writeFileSync(
+    'apps/mobile/android/app/versions.properties',
+    `versionCode=${versionCode}\nversionName=${version}`
+  );
 };
 
 const versionBumpChoices = [
@@ -62,34 +106,36 @@ inquirer.prompt(questions).then(async (answers) => {
   const versionBumpType: string = answers['version'];
   const appUpdateTypes: string[] = answers['apps'];
 
-  let newVersion = spawnSync('npm', [
+  const newVersion = spawnSync('npm', [
     'version',
     versionBumpType,
     '--git-tag-version=false',
   ])
     .stdout.toString()
-    .trim();
+    .trim()
+    .substr(1);
 
+  let gitTag = `v${newVersion}`;
   if (appUpdateTypes.length !== appUpdateChoices.length) {
-    newVersion += `+${appUpdateTypes.join('.')}`;
+    gitTag += `+${appUpdateTypes.join('.')}`;
   }
-  const commitMessage = newVersion.substr(1);
+  const commitMessage = gitTag.substr(1);
+
+  console.log('New version: ' + newVersion);
+  console.log('New tag: ' + gitTag);
 
   updateChangelog();
+  updateMobileAppVersions(newVersion);
   requireSuccessful(
     spawnSync('git', ['commit', '--all', '-m', `${commitMessage}`]),
     () => {
       console.error('Failed to create commit');
     }
   );
-  requireSuccessful(
-    spawnSync('git', ['tag', '-a', newVersion, '-m', '""']),
-    () => {
-      console.error('Failed to create tag', newVersion);
-    }
-  );
+  requireSuccessful(spawnSync('git', ['tag', '-a', gitTag, '-m', '""']), () => {
+    console.error('Failed to create tag', gitTag);
+  });
 
-  console.log('New version:' + newVersion);
   if (answers['push']) {
     requireSuccessful(
       spawnSync('git', ['push', '--follow-tags'], {
