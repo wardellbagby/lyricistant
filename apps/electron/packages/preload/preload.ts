76503ff8add858c88eeeaa25e43ece1d48860a18
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-03-01 20:54:42 -0800

    refactor: remove react-devtools from Electron
    Wasn't ever really used.

--- apps/electron/packages/preload/preload.ts
+++ apps/electron/packages/preload/preload.ts
@@ -1,13 +1,7 @@
 import log from 'electron-log';
 import { platformDelegate } from '@electron-delegates/Delegates';
-import { isDevelopment, isUnderTest } from '@lyricistant/common/BuildModes';
 
 process.on('loaded', () => {
   window.logger = log.functions;
   window.platformDelegate = platformDelegate;
-  if (isDevelopment || isUnderTest) {
-    import('react-devtools').catch((reason) =>
-      log.warn("Couldn't load React DevTools", reason)
-    );
-  }
 });
