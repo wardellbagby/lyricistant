afd9f48110e7074e8eb5b46c2e7ee2a6a7da27e2
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-06-02 10:37:34 -0700

    build: remove duplicate Workflow writing logic

--- scripts/create_workflows.ts
+++ scripts/create_workflows.ts
@@ -1,77 +1,21 @@
 #!/usr/bin/env -S node -r ./register-ts-node
 
-import fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml';
+import { dynamicImport } from 'tsimportlib';
 import { continuousIntegration } from '../.github/workflow-templates/continuousIntegration';
 import { generatePronunciations } from '../.github/workflow-templates/generatePronunciations';
-import { Job, Workflow } from '../.github/workflow-templates/helpers/Workflow';
 import { nightlyReleases } from '../.github/workflow-templates/nightlyReleases';
 import { productionReleases } from '../.github/workflow-templates/productionReleases';
 
-const fromEntries = <T>(entries: Array<[keyof T, T[keyof T]]>): T => {
-  const newObject = Object.create(null);
-  for (const [key, val] of entries) {
-    newObject[key] = val;
-  }
-  return newObject;
-};
-
-/**
- * A very complicated way to replace the "needs" field with the actual key of
- * the job being depended on.
- *
- * @param workflow The Workflow to normalize.
- */
-const normalize = (workflow: Workflow): Workflow => ({
-  ...workflow,
-  jobs: fromEntries(
-    Object.entries(workflow.jobs).map((namedJob) => {
-      const [name, job] = namedJob;
-      return [
-        name,
-        {
-          ...job,
-          needs: (Array.isArray(job.needs)
-            ? job.needs
-            : job.needs === undefined
-            ? undefined
-            : [job.needs]
-          )?.map((need: string | Job) => {
-            if (typeof need === 'string') {
-              return need;
-            } else {
-              const reference = Object.entries(workflow.jobs).find(
-                (match) => match[1] === need
-              );
-              return reference[0];
-            }
-          }),
-        },
-      ];
-    })
-  ),
-});
-const writeWorkflow = (workflow: Workflow) => {
-  const file = path.resolve(
-    '.github/workflows',
-    `${workflow.name.replace(/\s/g, '-')}.yml`
-  );
-  workflow = normalize(workflow);
-  fs.writeFileSync(
-    file,
-    yaml.dump(workflow, { forceQuotes: true, noRefs: true })
-  );
-};
-const templates = [
+const workflows = [
   continuousIntegration,
   nightlyReleases,
   productionReleases,
   generatePronunciations,
 ];
 
-const createWorkflows = () => {
-  templates.forEach(writeWorkflow);
-};
-
-createWorkflows();
+type GhWorkflowGen = typeof import('@wardellbagby/gh-workflow-gen');
+dynamicImport('@wardellbagby/gh-workflow-gen', module).then(
+  ({ writeWorkflow }: GhWorkflowGen) => {
+    workflows.forEach((workflow) => writeWorkflow(workflow));
+  }
+);
