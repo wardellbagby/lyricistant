535211f69f1fcb82817639e2ba0d7a72260d8971
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2022-12-10 17:34:23 -0800

    test: lower findBy query timeout in UI tests

--- tooling/browser-test-setup.js
+++ tooling/browser-test-setup.js
@@ -2,5 +2,5 @@ const { configure } = require('playwright-testing-library');
 
 configure({
   // Use a longer timeout for the findBy queries on CI since, ya know, it's slow.
-  asyncUtilTimeout: process.env.CI ? 10_000 : 1_000,
+  asyncUtilTimeout: process.env.CI ? 5_000 : 1_000,
 });
