e425525059c415502bfca2f9e1059dc14731fbcc
Wardell Bagby <7834478+wardellbagby@users.noreply.github.com>
2023-07-07 22:06:40 -0700

    build: bump capacitor to v5
    Also updates our default macOS runner to run macOS 13 since Xcode 14 is
    required to build.

--- .github/workflow-templates/Runners.ts
+++ .github/workflow-templates/Runners.ts
@@ -1,11 +1,11 @@
 import { Runners } from '@wardellbagby/gh-workflow-gen';
 
 /** The GitHub Actions runner that should be used by the vast majority of actions. */
-export const defaultRunner: Runners = 'ubuntu-20.04';
+export const defaultRunner: Runners = 'ubuntu-22.04';
 
 /**
  * The GitHub Actions runner that should be used when a Job has a dependency on
  * macOS. E.g., has to build the iOS app or has to build the macOS flavor of the
  * Electron app.
  */
-export const defaultMacOsRunner: Runners = 'macos-12';
+export const defaultMacOsRunner: Runners = 'macos-13';
